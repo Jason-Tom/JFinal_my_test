39935e0318c7a653c392659378ba4db27df8f675
Jason-Tom <429295712@qq.com>
2016-03-14 11:16:54 +0800

    src文件不行重新新建文件夹

---
+++ src/demo/DemoConfig.java
@@ -0,0 +1,23 @@
+package demo;
+
+import com.jfinal.config.*;
+
+public class DemoConfig extends JFinalConfig {
+	public void configConstant(Constants me) {
+		me.setDevMode(true);
+	}
+
+	public void configRoute(Routes me) {
+		me.add("/", HelloController.class);//一定要建根路径
+		me.add("/hello", HelloController.class);
+	}
+
+	public void configPlugin(Plugins me) {
+	}
+
+	public void configInterceptor(Interceptors me) {
+	}
+
+	public void configHandler(Handlers me) {
+	}
+}
